706dff9398cf1ed1f8b9d6a9f43b9871bf1c751b
Alexandra Wolf <14823725+The-Catsby@users.noreply.github.com>
2019-04-23 12:43:09 -0600

    backend - added comments to models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-
+#################################################
+## Name: Player
+#################################################
 class Player(models.Model):
     name = models.CharField(max_length=50, null=True)
     date = models.DateField(auto_now=True)
@@ -9,23 +11,39 @@ class Player(models.Model):
     def __str__(self):
         return self.name
 
+#################################################
+## Name: Item
+#################################################
 class Item(models.Model):
     name = models.CharField(max_length=32)
 
     def __str__(self):
         return self.name
-        
+
+#################################################
+## Name: Room
+#################################################
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+#################################################
+## Name: Status
+#################################################
 class Status(models.Model):
     message = models.CharField(max_length=4000)
     time = models.CharField(max_length=50, blank=True)
     game = models.CharField(max_length=50, blank=True)
     def __str__(self):
         return self.message
-        
+
+#################################################
+## Name: Test
+#################################################
 class Test(models.Model):
     test = models.CharField(max_length=32)
 
     def __str__(self):
         return self.test
-
-
